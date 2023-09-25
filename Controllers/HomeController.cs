7ad2fb4e58af887e26b61ed834ada06d5a9fcac7
Niyazi <berataktas2834@gmail.com>
2023-09-25 23:00:30 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using E_ticaretMVC.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_ticaretMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext _context = new DataContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_context.Products.Where(i => i.IsHome && i.IsApproved).ToList());
+        }
+        public ActionResult Details(int id)
+        {
+            return View(_context.Products.Where(i => i.Id == id).ToList());
+        }
+        public ActionResult List()
+        {
+            return View(_context.Products.ToList());
+        }
+
+    }
+}
---
+++ Entity/DataInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_ticaretMVC.Entity
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var Kategoriler = new List<Category>() 
+            {
+                new Category() { Name = "Kamera", Description = "Kamera ürünleri"},
+                new Category() { Name = "Bilgisyar", Description = "Bilgisayar ürünleri"},
+                new Category() { Name = "Elektronik", Description = "Elektronik ürünleri"},
+                new Category() { Name = "Telefon", Description = "Telefon ürünleri"},
+                new Category() { Name = "Beyaz Eşya", Description = "Beyaz Eşya ürünleri"},
+            };
+            foreach (var kategori in Kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+            context.SaveChanges();
+            var urunler = new List<Product>() 
+            {
+                new Product(){Name="test",Description="deneme",Price=1200, Stock=10, IsApproved=true, CategoryId=1, IsHome=true},
+                new Product(){Name="deneme",Description="deneme",Price=1000, Stock=10, IsApproved=true, CategoryId=1, IsHome=true},
+                new Product(){Name="test",Description="test",Price=100, Stock=20, IsApproved=true, CategoryId=1, IsHome=true},
+                new Product(){Name="test",Description="deneme",Price=1200, Stock=5, IsApproved=true, CategoryId=1, IsHome = true},
+            };
+            foreach (var urun in urunler)
+            {
+                context.Products.Add(urun);
+            }
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
